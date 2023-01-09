a752656963342c2ac0d633a9cd6e8b92faaa98c7
Hieu Pham <pmh.ars14@gmail.com>
2023-01-09 10:33:17 +0700

    run lua from init.vim

--- lua/k4ttitude/init.lua
+++ lua/k4ttitude/init.lua
@@ -1,4 +1,5 @@
-require("k4ttitude.packer")
-require("k4ttitude.set")
 require("k4ttitude.keymap")
-
+if vim.g.vscode == nil then
+  require("k4ttitude.packer")
+  require("k4ttitude.set")
+end
--- lua/k4ttitude/keymap.lua
+++ lua/k4ttitude/keymap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
@@ -12,7 +12,6 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("v", "J", ":m .+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m .-2<CR>gv=gv")
 
-vim.cmd.colorscheme "catppuccin"
 --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
