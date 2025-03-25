6d4142f9705bff2f5240691f66f54e8cb0abff13
Pham Minh Hieu <pmh.ars14@gmail.com>
2025-03-25 18:18:42 +0700

    console.log macro

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,15 @@ vim.g.lazyvim_prettier_needs_config = true
 -- avante
 -- views can only be fully collapsed with the global statusline
 vim.opt.laststatus = 3
+
+-- Macros
+local esc = "\27" -- Escape
+
+vim.api.nvim_create_augroup("JSLogMacro", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = "JSLogMacro",
+  pattern = { "javascript", "typescript" },
+  callback = function()
+    vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la, " .. esc .. "pla" .. esc .. "'")
+  end,
+})
