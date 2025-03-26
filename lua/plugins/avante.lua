bb7f829ba708c333274d9d19cfc55ff92266f718
Pham Minh Hieu <pmh.ars14@gmail.com>
2025-03-26 15:14:27 +0700

    remove harppon2, telescope

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -4,14 +4,20 @@ return {
   lazy = false,
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
-    -- add any opts here
-    provider = "claude_3_7_sonnet", -- Recommend using Claude
+    provider = "claude_3_5_sonnet",
     -- auto_suggestions_provider = "claude",
     vendors = {
       claude_3_5_haiku = {
         __inherited_from = "claude",
         endpoint = "https://api.anthropic.com",
         model = "claude-3-5-haiku-20241022",
+        temperature = 0,
+        max_tokens = 4096,
+      },
+      claude_3_5_sonnet = {
+        __inherited_from = "claude",
+        endpoint = "https://api.anthropic.com",
+        model = "claude-3-5-sonnet-20241022",
         temperature = 0,
         max_tokens = 4096,
       },
@@ -50,20 +56,19 @@ return {
       },
     },
   },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "echasnovski/mini.pick", -- for file_selector provider mini.pick
     "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "echasnovski/mini.icons",
     -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+    -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
@@ -76,8 +81,6 @@ return {
           drag_and_drop = {
             insert_mode = true,
           },
-          -- required for Windows users
-          -- use_absolute_path = true,
         },
       },
     },
