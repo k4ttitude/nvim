3e6031a6c8437aad5acd26d906e191ffab731499
Pham Minh Hieu <pmh.ars14@gmail.com>
2025-04-01 17:11:17 +0700

    obsidian: disable frontmatter

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -3,20 +3,12 @@ return {
   version = "*", -- recommended, use latest release instead of latest commit
   event = "VeryLazy",
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
   dependencies = {
-    -- Required.
     "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
   },
   opts = {
+    disable_frontmatter = true,
+    ui = { enable = false },
     workspaces = {
       {
         name = "personal",
@@ -27,6 +19,7 @@ return {
   keys = {
     { "<leader>os", "<cmd>ObsidianSearch<cr>", desc = "Obsidian Search" },
     { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "Obsidian New" },
+    { "<leader>ot", "<cmd>ObsidianToday<cr>", desc = "Obsidian Today" },
     { "<leader>o<tab>", ":Obsidian", desc = "Obsidian..." },
   },
 }
