fb15abd493b5130969138eb8200be744197d8581
k4ttitude <pmh.ars14@gmail.com>
2025-04-03 23:00:27 +0700

    disable obsidian on linux

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,25 +1,27 @@
-return {
-  "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
-  event = "VeryLazy",
-  ft = "markdown",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  opts = {
-    disable_frontmatter = true,
-    ui = { enable = false },
-    workspaces = {
-      {
-        name = "personal",
-        path = "/Users/kattitude/Library/Mobile Documents/iCloud~md~obsidian/Documents/Root",
+if vim.loop.os_uname().sysname == "Darwin" then
+  return {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    event = "VeryLazy",
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      disable_frontmatter = true,
+      ui = { enable = false },
+      workspaces = {
+        {
+          name = "personal",
+          path = "/Users/kattitude/Library/Mobile Documents/iCloud~md~obsidian/Documents/Root",
+        },
       },
     },
-  },
-  keys = {
-    { "<leader>os", "<cmd>ObsidianSearch<cr>", desc = "Obsidian Search" },
-    { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "Obsidian New" },
-    { "<leader>ot", "<cmd>ObsidianToday<cr>", desc = "Obsidian Today" },
-    { "<leader>o<tab>", ":Obsidian", desc = "Obsidian..." },
-  },
-}
+    keys = {
+      { "<leader>os", "<cmd>ObsidianSearch<cr>", desc = "Obsidian Search" },
+      { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "Obsidian New" },
+      { "<leader>ot", "<cmd>ObsidianToday<cr>", desc = "Obsidian Today" },
+      { "<leader>o<tab>", ":Obsidian", desc = "Obsidian..." },
+    },
+  }
+end
