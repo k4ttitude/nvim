225d89a5fc2e00b24c593f02bee14812e9a068df
k4ttitude <pmh.ars14@gmail.com>
2025-06-26 23:18:08 +0700

    change keymap bc -> by

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,7 +6,7 @@ local map = LazyVim.safe_keymap_set
 map("n", "<c-u>", "<c-u>zz")
 map("n", "<c-d>", "<c-d>zz")
 
-map("n", "<leader>bc", function()
+map("n", "<leader>by", function()
   local current_file = vim.fn.expand("%")
   vim.fn.setreg("+", current_file)
   vim.notify("Cpoied: " .. current_file, vim.log.levels.INFO)
