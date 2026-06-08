7fe32a7f13dacc366074f16a247ddfe33a6b8e39
k4ttitude <pmh.ars14@gmail.com>
2026-06-08 08:30:50 +0700

    fix copy path

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,7 +7,15 @@ map("n", "<c-u>", "<c-u>zz")
 map("n", "<c-d>", "<c-d>zz")
 
 map("n", "<leader>by", function()
-  local current_file = vim.fn.expand("%")
-  vim.fn.setreg("+", current_file)
-  vim.notify("Cpoied: " .. current_file, vim.log.levels.INFO)
+  local abs_path = vim.fn.expand("%:p")
+  -- let git resolve the outermost working tree (handles submodules);
+  -- fall back to LazyVim's root (lsp/cwd) when not in a git repo
+  local out = vim.fn.systemlist({
+    "git", "-C", vim.fn.expand("%:p:h"),
+    "rev-parse", "--show-superproject-working-tree", "--show-toplevel",
+  })
+  local root = vim.v.shell_error == 0 and out[1] or LazyVim.root()
+  local rel_path = vim.fs.relpath(root, abs_path) or vim.fn.fnamemodify(abs_path, ":.")
+  vim.fn.setreg("+", rel_path)
+  vim.notify("Copied: " .. rel_path, vim.log.levels.INFO)
 end, { desc = "Copy Buffer path" })
